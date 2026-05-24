e0b59a2f5da90946dacef193e3e24c2f34f1f42a
Rishabh Bahuguna <rishabhbahuguna2026@gmail.com>
2026-05-24 12:57:32 +0530

    more changes

--- src/main/java/com/example/UberReviewService/Services/ReviewServiceImpl.java
+++ src/main/java/com/example/UberReviewService/Services/ReviewServiceImpl.java
@@ -3,6 +3,9 @@ package com.example.UberReviewService.Services;
 
 import com.example.UberReviewService.Models.Review;
 import com.example.UberReviewService.Repositories.ReviewRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.hibernate.FetchNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,27 +22,55 @@ public class ReviewServiceImpl implements ReviewService {
 
 
     @Override
-    public Optional<Review> findReviewById(Long Id) {
-        return null;
+    public Optional<Review> findReviewById(Long id) throws EntityNotFoundException {
+        Optional<Review> review;
+        try {
+            review = this.reviewRepository.findById(id);
+            if (review.isEmpty()) {
+                throw new EntityNotFoundException("Review with id " + id + " not found");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            if(e.getClass() == EntityNotFoundException.class){
+                throw new FetchNotFoundException("Review with id " + id + " not found", id);
+            }
+            throw new FetchNotFoundException("Unable to fetch, PLease try again later!", id);
+        }
+        return review;
     }
 
     @Override
     public List<Review> findAllReviews() {
-        return null;
+        return this.reviewRepository.findAll();
     }
 
     @Override
-    public boolean deleteReviewById(Long Id) {
-        return false;
+    public boolean deleteReviewById(Long id) {
+        try {
+            Review review = this.reviewRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+            this.reviewRepository.delete(review);
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
+    @Transactional
     public Review publishReview(Review review) {
-        return null;
+        return this.reviewRepository.save(review);
     }
 
     @Override
-    public Review updateReview(Long id, Review review) {
-        return null;
+    public Review updateReview(Long id, Review newReviewData) {
+        Review review = this.reviewRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        if(newReviewData.getRating() != null){
+            review.setRating(newReviewData.getRating());
+        }
+        if(newReviewData.getContent() != null){
+            review.setContent(newReviewData.getContent());
+        }
+        return this.reviewRepository.save(review);
     }
 }
