5307c2f3f0ab7eb5b20aa6fd6eb30822177c3587
Rishabh Bahuguna <rishabhbahuguna2026@gmail.com>
2026-06-05 20:21:21 +0530

    Added more changes

--- src/main/resources/db/migration/V5__shifting_mapping_from_booking_to_review.sql
+++ src/main/resources/db/migration/V5__shifting_mapping_from_booking_to_review.sql
@@ -1,13 +1,1 @@
-ALTER TABLE booking
-    DROP
-        FOREIGN KEY FK_BOOKING_ON_REVIEW;
-
-ALTER TABLE booking_review
-    ADD booking_id BIGINT NULL;
-
-ALTER TABLE booking_review
-    ADD CONSTRAINT FK_BOOKING_REVIEW_ON_BOOKING FOREIGN KEY (booking_id) REFERENCES booking (id);
-
-ALTER TABLE booking
-    DROP
-        COLUMN review_id;
+-- Migration already applied manually. Skipping.
